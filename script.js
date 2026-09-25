3d10b721c139e9f0530cfff7f37a95a708d1f5e5
saswataffiliates-png <saswat.affiliates@gmail.com>
2026-09-25 16:12:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-const fruitButton = document.querySelector("#fruitButton");
+const nameInput = document.querySelector("#nameInput");
+
+const ageInput = document.querySelector("#ageInput");
+
+const submitButton = document.querySelector("#submitButton");
 
 const message = document.querySelector("#message");
 
-fruitButton.addEventListener("click", showFruits);
+submitButton.addEventListener("click", showPerson);
 
-function showFruits() {
+function showPerson() {
 
-    const fruits = [
-        "Apple",
-        "Mango",
-        "Banana",
-        "Orange"
-    ];
+    const name = nameInput.value;
 
-    message.textContent = fruits.join(" | ");
+    const age = ageInput.value;
+
+    if (name === "" || age === "") {
+
+        message.textContent =
+            "Please enter both your name and age.";
+
+    } else {
+
+        const person = {
+            name: name,
+            age: age
+        };
+
+        message.textContent =
+            "Hello " + person.name +
+            "! You are " + person.age +
+            " years old.";
+
+    }
 
 }
