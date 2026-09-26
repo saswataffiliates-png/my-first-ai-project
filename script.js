2f8ed738f762595f06dcf3785e712fd7c2bcc4c0
saswataffiliates-png <saswat.affiliates@gmail.com>
2026-09-26 14:00:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-async function getProducts() {
+const loadButton = document.querySelector("#loadButton");
 
-    const response = await fetch(
-        "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
-    );
+const message = document.querySelector("#message");
 
-    if (!response.ok) {
-        throw new Error("Could not get the data");
+
+loadButton.addEventListener("click", getProduct);
+
+
+async function getProduct() {
+
+    message.textContent = "Loading...";
+
+
+    try {
+
+        const response = await fetch(
+            "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+        );
+
+
+        if (!response.ok) {
+
+            throw new Error("Could not get the data");
+
+        }
+
+
+        const data = await response.json();
+
+
+        message.textContent =
+            "Product: " + data[0].name;
+
+
+    } catch (error) {
+
+        message.textContent =
+            "Something went wrong.";
+
+        console.error(error);
+
     }
 
-    const data = await response.json();
-
-    console.log(data);
 }
-
-getProducts();
